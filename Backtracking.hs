c2fff078955b5b0c108ca361012f1c355f3630f7
Dmitry Shlagoff <richyannet@yandex.ru>
2019-03-27 17:02:21 +0700

    Backtracking tutorial

---
+++ Backtracking.hs
@@ -0,0 +1,47 @@
+-- import Prelude hiding ((>>=), return)
+import Control.Monad
+
+type Choice a = [a]
+
+choose :: [a] -> Choice a
+choose xs = xs
+
+pair456 :: Int -> Choice (Int,Int)
+pair456 x = choose [(x,4), (x,5), (x,6)]
+
+-- join :: Choice (Choice a) -> Choice a
+-- join = concat
+
+-- (>>=) :: Choice a -> (a -> Choice b) -> Choice b
+-- choices >>= f = join (map f choices)
+
+-- return :: a -> Choice a
+-- return x = choose [x]
+
+-- makePairs :: Choice (Int, Int)
+-- makePairs = join (map (\x -> join (map (\y -> join (map (\z -> return (x, y)) $ choose [])) $ choose [4,5,6])) $ choose [1,2,3])
+-- makePairs =
+--  choose [1,2,3] >>= (\x ->
+--  choose [4,5,6] >>= (\y ->
+--  return (x, y)))
+
+-- makePairs = do
+--  x <- choose [1,2,3]
+--  y <- choose [4,5,6]
+--  return (x, y)
+
+-- mzero :: Choice a
+-- mzero = choose []
+
+guard :: Bool -> Choice ()
+guard True = return ()
+guard False = mzero
+
+-- solveConstraint = do
+--  x <- choose [1,2,3]
+--  y <- choose [4,5,6]
+--  guard (x*y == 8)
+--  return (x, y)
+
+maybeExample = join (fmap (\x -> join (fmap (\y -> return (x, y)) $ Just 2)) $ Nothing)
+
